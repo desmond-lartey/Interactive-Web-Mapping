e337f1d26a5c62f0b5e452273e2c5b5c2dd5e99e
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-09-17 23:58:02 +0200

    add population explorer

---
+++ populations.py
@@ -0,0 +1,20 @@
+import geopandas as gpd
+import leafmap.foliumap as leafmap
+
+def population_and_capitals_app():
+    st.title("World Population and Capitals")
+
+    # Using geopandas to fetch the datasets
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    capitals = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
+
+    # Plotting the map
+    m = leafmap.Map(center=[20, 0], zoom=2)
+
+    # Add the countries layer with population as popup
+    m.add_gdf(world, layer_name="Countries", popup=["name", "pop_est"])
+
+    # Add the capitals layer
+    m.add_gdf(capitals, layer_name="Capitals", popup="name", marker_type="marker")
+
+    m.to_streamlit(height=700)
