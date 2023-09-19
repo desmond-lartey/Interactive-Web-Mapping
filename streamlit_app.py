a3b5788b0396dddaa0c060edcc1dc36831ffc080
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-09-19 18:47:22 +0200

    edited

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 # A dictionary of apps in the format of {"App title": "App icon"}
 # More icons can be found here: https://icons.getbootstrap.com
 
+
 apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
     {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
