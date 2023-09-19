3af36f765fbacbcbaa0890545aafb78b0c9da43f
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-09-19 18:45:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ apps = [
     {"func": vector.app, "title": "Vector", "icon": "bounding-box"},
     {"func": populations.app, "title": "Population & Capitals", "icon": "globe"},
     {"func": worldbank.app, "title": "Population Stats", "icon": "globe"},
-    {"func": urbangreen.app, "title": "Population Stats", "icon": "globe"}
+    {"func": urbangreen.app, "title": "urbangreen", "icon": "globe"}
 ]
 
 
