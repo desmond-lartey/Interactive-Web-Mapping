233126dfeb32e178193680c6d2225d66a14131cc
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-12-10 22:07:29 +0100

    Add files via upload

---
+++ apps/carbon2.py
@@ -0,0 +1,108 @@
+import streamlit as st
+import requests
+import pandas as pd
+import plotly.express as px
+
+# Function for Climatiq API Integration
+def calculate_transportation_emissions(api_key, route, vehicle_type, vehicle_weight):
+    url = "https://beta4.api.climatiq.io/freight/intermodal"
+    headers = {'Authorization': f'Bearer {api_key}'}
+    data = {
+        "route": route,
+        "cargo": {
+            "weight": vehicle_weight,
+            "weight_unit": "t"
+        }
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            st.error(f"API Error: {response.status_code}")
+            return None
+    except Exception as e:
+        st.error(f"Error fetching data: {e}")
+        return None
+
+# Function to plot emissions data
+def plot_emissions(data):
+    fig = px.bar(data, x="Category", y="Emissions", title="Your Carbon Emissions Breakdown")
+    return fig
+
+def app():
+    st.title("Global Carbon Calculator App")
+
+    # Original Carbon Footprint Calculation
+    st.header("Calculate Your Carbon Footprint")
+
+    country = st.selectbox("Select Your Country", ["India"])  # Add more countries as needed
+    col1, col2 = st.columns(2)
+
+    with col1:
+        distance = st.slider("🚗 Daily commute distance (in km)", 0.0, 100.0, key="distance_input")
+        electricity = st.slider("💡 Monthly electricity consumption (in kWh)", 0.0, 1000.0, key="electricity_input")
+
+    with col2:
+        waste = st.slider("🗑️ Weekly waste generated (in kg)", 0.0, 100.0, key="waste_input")
+        meals = st.number_input("🍽️ Daily number of meals", 0, key="meals_input")
+
+    normalized_inputs = {
+        "distance": distance * 365,
+        "electricity": electricity * 12,
+        "meals": meals * 365,
+        "waste": waste * 52
+    }
+
+    EMISSION_FACTORS = {
+        "India": {
+            "Transportation": 0.14,
+            "Electricity": 0.82,
+            "Diet": 1.25,
+            "Waste": 0.1
+        }
+    }
+
+    if st.button("Calculate Personal CO2 Emissions"):
+        factors = EMISSION_FACTORS[country]
+        transportation_emissions = factors["Transportation"] * normalized_inputs["distance"]
+        electricity_emissions = factors["Electricity"] * normalized_inputs["electricity"]
+        diet_emissions = factors["Diet"] * normalized_inputs["meals"]
+        waste_emissions = factors["Waste"] * normalized_inputs["waste"]
+
+        total_emissions = round(sum([transportation_emissions, electricity_emissions, diet_emissions, waste_emissions]) / 1000, 2)
+        st.subheader("Your Total Carbon Footprint")
+        st.metric(label="Total Emissions", value=f"{total_emissions} tonnes CO2 per year")
+
+        # Data for plot
+        emissions_data = pd.DataFrame({
+            "Category": ["Transportation", "Electricity", "Diet", "Waste"],
+            "Emissions": [transportation_emissions, electricity_emissions, diet_emissions, waste_emissions]
+        })
+        st.plotly_chart(plot_emissions(emissions_data))
+
+    # Climatiq API Integration for Transportation Emissions
+    st.header("Calculate Transportation Emissions (Climatiq API)")
+
+    start_location = st.text_input("Start Location (e.g., Hamburg)")
+    end_location = st.text_input("End Location (e.g., Berlin)")
+    vehicle_type = st.selectbox("Vehicle Type", ["van", "truck", "air", "sea"])
+    vehicle_weight = st.number_input("Vehicle Weight (in tonnes)", min_value=0.1, step=0.1)
+
+    route = [
+        {"location": {"query": start_location}},
+        {"transport_mode": vehicle_type, "leg_details": {"vehicle_type": vehicle_type, "vehicle_weight": f"lte_{vehicle_weight}t"}},
+        {"location": {"query": end_location}}
+    ]
+
+    API_KEY = 'YN8AMDGBFNMGZ9P1DX711NQGK946'  # Replace with a secure method of storing the API key
+
+    if st.button("Calculate Transportation Emissions (API)"):
+        emission_data = calculate_transportation_emissions(API_KEY, route, vehicle_type, vehicle_weight)
+        
+        if emission_data:
+            st.write("Total CO2 Emissions for the trip:", emission_data.get('co2e', 0), "kg CO2e")
+            st.write("Total distance traveled:", emission_data.get('distance_km', 0), "km")
+        else:
+            st.write("Unable to calculate emissions. Please check input values.")
