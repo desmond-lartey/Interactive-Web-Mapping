571bce650d4c0ec8628f72cd7058d08d2c0fe73f
Desmond Lartey <107281765+desmond-lartey@users.noreply.github.com>
2023-12-10 23:21:02 +0100

    Update ClimateAPP.py

--- ClimateAPP.py
+++ ClimateAPP.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, heatmap, upload, vector, populations, worldbank, climate,carbon2 #urbangreen,climate  # import your app modules here
+from apps import home, upload, vector, populations, worldbank, climate,carbon2, heatmap, climate  #urbangreen #import your app modules here
 
 st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
